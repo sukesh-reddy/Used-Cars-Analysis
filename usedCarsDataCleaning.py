b8f97646014c85ff380f22baefc396ceb21cd8c4
Ajith Kumar V S <avakkalagantisunilku@hawk.iit.edu>
2020-03-17 14:13:05 -0500

    Updated Code

--- usedCarsDataCleaning.py
+++ usedCarsDataCleaning.py
@@ -1,7 +1,8 @@
 import pandas as pd
-used_cars=pd.read_csv("C:/Users/avakk/Downloads/vehicles.csv")
+used_cars=pd.read_csv("C:/Users/avakk/Downloads/vehicles.csv")  #Reading CSV
 
 used_cars.shape
 used_cars.info()
-used_cars.isnull().sum()
-used_cars.drop(["county"],axis=1,inplace=True)
+used_cars.isnull().sum() 
+used_cars.drop(["region_url"],axis=1,inplace=True) #Does not contribute towards prediction
+used_cars.drop(["county"],axis=1,inplace=True) #High percentage of null values
