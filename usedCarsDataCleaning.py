26af44716159f4f62ed0292484fd660470e3fac7
Ajith Kumar V S <avakkalagantisunilku@hawk.iit.edu>
2020-03-18 00:01:18 -0500

    Added Heatmaps

--- usedCarsDataCleaning.py
+++ usedCarsDataCleaning.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import seaborn as sns
 used_cars=pd.read_csv("C:/Users/avakk/Downloads/vehicles.csv")  #Reading CSV
 
 used_cars.shape
 used_cars.info()
+sns.heatmap(used_cars.isnull())
 used_cars.isnull().sum() 
 used_cars.drop(["region_url"],axis=1,inplace=True) #Does not contribute towards prediction
 used_cars.drop(["county"],axis=1,inplace=True) #High percentage of null values
